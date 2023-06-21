96f30e2168262384d95d4dd3e27197beeb736879
Leandro Braga <leandro.quadros@gmail.com>
2023-06-21 01:32:53 +0000

    update main to use jsonlog

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,12 +5,12 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/leandrobraga/greenlight/internal/data"
+	"github.com/leandrobraga/greenlight/internal/jsonlog"
 	_ "github.com/lib/pq"
 )
 
@@ -26,7 +26,7 @@ type config struct {
 
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
@@ -38,15 +38,15 @@ func main() {
 	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://leandro:123@db/greenlight?sslmode=disable", "PostgreSQL DSN")
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
 	db, err := openDB(cfg)
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err, nil)
 	}
 	defer db.Close()
 
-	logger.Printf("database connection pool established")
+	logger.PrintInfo("database connection pool established", nil)
 
 	app := &application{
 		config: cfg,
@@ -62,9 +62,9 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 
-	logger.Printf("Starting %s server on port %s", cfg.env, srv.Addr)
+	logger.PrintInfo("Starting server", map[string]string{"addr": srv.Addr, "env": cfg.env})
 	err = srv.ListenAndServe()
-	logger.Fatal(err)
+	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
