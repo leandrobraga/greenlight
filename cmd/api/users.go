5fe2881d2a9d879932b697f0037829ef2d3c2177
Leandro Braga <leandro.quadros@gmail.com>
2023-07-06 19:08:31 +0000

    Send email in background

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/leandrobraga/greenlight/internal/data"
@@ -52,13 +53,24 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	err = app.mailer.Sender(user.Email, "user_welcome.tmpl", user)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
+	go func() {
+		// caso tenha algum panic ele captura o erro e põe no log
+		// e não mata a aplicação
+		defer func() {
+			if err := recover(); err != nil {
+				app.logger.PrintError(fmt.Errorf("%s", err), nil)
+			}
+		}()
+		err = app.mailer.Sender(user.Email, "user_welcome.tmpl", user)
+		if err != nil {
+			app.logger.PrintError(err, nil)
+			return
+		}
+	}()
 
-	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	// Status Accepted (202) indicates that the request has been accepted for processing,
+	// but the processing has not been completed
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
